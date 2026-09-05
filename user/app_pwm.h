70a003d84ab639b50de603c42b6be9901a6e1016
kingdome55 <2570573996@qq.com>
2026-09-05 15:32:56 +0800

    feat: 阶段三，PWM 控制电机速度与转向

--- user/app_pwm.h
+++ user/app_pwm.h
@@ -5,6 +5,9 @@
 
 void App_PWM_Init(void);
 void App_PWM_Cmd(uint8_t on);
+void App_PWM_Set_L(float Duty);
+void App_PWM_Set_R(float Duty);
+
 
 
 #endif
--- user/main.c
+++ user/main.c
@@ -1,14 +1,24 @@
 #include "stm32f10x.h"
+#include "app_bat.h"
+#include "app_pwm.h"
+#include "app_button.h"
+#include "pwm_test.h"
 #include "bat_test.h"
-
 int main(void)
 {
   NVIC_PriorityGroupConfig(NVIC_PriorityGroup_0);
   
-  Bat_Test();
+//Bat_Test();
+  PWM_Test();
+  App_Bat_Init();
+  App_Button_Init();
+  App_PWM_Init();
+  
   
 	while(1)
   {
+    App_Bat_Proc();
+    App_Bat_Proc();
     
 	}
 }
